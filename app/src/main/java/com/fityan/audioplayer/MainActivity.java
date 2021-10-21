a882a578d5820d79d11baf3c1a9f6c2ac8ffc1f3
Fityandhiya Islam Nugroho <fityandhiya@gmail.com>
2021-10-21 16:17:20 +0700

    v1.0.0: One audio only

---
+++ app/src/main/java/com/fityan/audioplayer/MainActivity.java
@@ -0,0 +1,161 @@
+package com.fityan.audioplayer;
+
+import android.annotation.SuppressLint;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.concurrent.TimeUnit;
+
+public class MainActivity extends AppCompatActivity {
+    /* View properties */
+    TextView tvAudioName, tvAudioPosition, tvAudioDuration;
+    SeekBar seekBarAudio;
+    ImageButton btnPrevious, btnNext, btnPlay, btnPause;
+
+    MediaPlayer mediaPlayer;
+    Handler handler = new Handler();
+    Runnable runnable;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        /* Assign view properties */
+        tvAudioName = findViewById(R.id.tvAudioName);
+        tvAudioPosition = findViewById(R.id.tvAudioPosition);
+        tvAudioDuration = findViewById(R.id.tvAudioDuration);
+        seekBarAudio = findViewById(R.id.seekBarAudio);
+        btnPrevious = findViewById(R.id.btnPrevious);
+        btnNext = findViewById(R.id.btnNext);
+        btnPlay = findViewById(R.id.btnPlay);
+        btnPause = findViewById(R.id.btnPause);
+
+
+        /* Initialize media player */
+        mediaPlayer = MediaPlayer.create(this, R.raw.maroon5_girls_like_you);
+
+        /* Initialize runnable */
+        runnable = new Runnable() {
+            @Override
+            public void run() {
+                /* Set progress on seek bar */
+                seekBarAudio.setProgress(mediaPlayer.getCurrentPosition());
+
+                /* Handler post delay for 0.5s */
+                handler.postDelayed(this, 500);
+
+                /* Update the current position on display */
+                tvAudioPosition.setText(seekBarTimeFormat(
+                        mediaPlayer.getCurrentPosition()));
+            }
+        };
+
+        /* Get duration of media player,
+            Convert ms to minutes & seconds, then displaying it */
+        tvAudioDuration.setText(seekBarTimeFormat(mediaPlayer.getDuration()));
+
+
+        /* When Play Button is clicked */
+        btnPlay.setOnClickListener(view -> {
+            /* Hide the Play Button & show the Pause Button */
+            showPauseButton();
+
+            /* Start the media player */
+            mediaPlayer.start();
+
+            /* Set seek bar max */
+            seekBarAudio.setMax(mediaPlayer.getDuration());
+
+            /* Start handler */
+            handler.postDelayed(runnable, 0);
+        });
+
+
+        /* When Pause Button is clicked */
+        btnPause.setOnClickListener(view -> {
+            /* Hide the Pause Button & show the Play Button */
+            showPlayButton();
+
+            /* Pause the media player */
+            mediaPlayer.pause();
+
+            /* Stop handler */
+            handler.removeCallbacks(runnable);
+        });
+
+
+        /* Previous Button */
+        btnPrevious.setOnClickListener(view -> {
+        });
+
+
+        /* Next Button */
+        btnNext.setOnClickListener(view -> {
+        });
+
+
+        /* When Seek Bar is scrolled */
+        seekBarAudio.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                if (b) {
+                    /* When drag on the seek bar, set progress to the seek bar */
+                    mediaPlayer.seekTo(i);
+
+                    /* Update the current position on display */
+                    tvAudioPosition.setText(seekBarTimeFormat(
+                            mediaPlayer.getCurrentPosition()));
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        });
+
+
+        mediaPlayer.setOnCompletionListener(mediaPlayer -> {
+            /* Hide the Pause Button & show the Play Button  */
+            showPlayButton();
+
+            /* Reset media player position */
+            mediaPlayer.seekTo(0);
+        });
+    }
+
+
+    @SuppressLint("DefaultLocale")
+    private String seekBarTimeFormat(int durationInMs) {
+        long minutesDuration = TimeUnit.MILLISECONDS.toMinutes(durationInMs);
+        long secondsDuration = TimeUnit.MILLISECONDS.toSeconds(durationInMs);
+
+        return String.format("%02d:%02d",
+                minutesDuration,
+                secondsDuration - TimeUnit.MINUTES.toSeconds(minutesDuration));
+    }
+
+
+    private void showPlayButton() {
+        btnPlay.setVisibility(View.VISIBLE);
+        btnPause.setVisibility(View.GONE);
+    }
+
+
+    private void showPauseButton() {
+        btnPause.setVisibility(View.VISIBLE);
+        btnPlay.setVisibility(View.GONE);
+    }
+}
